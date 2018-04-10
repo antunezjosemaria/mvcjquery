292d79dc565e835e20c6e3f14382d743d899215e
Jose Maria Antunez <jose.antunez@globant.com>
2018-04-10 13:35:59 -0300

    Subo proyecto MVCJquery

---
+++ MVCJquery/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using MVCJquery.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MVCJquery.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCTutorialEntities db = new MVCTutorialEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetStudentList()
+        {
+            List<StudentViewModel> StudentList = db.tblStudents.Where(x => x.IsDelete == false).Select(x => new StudentViewModel
+            {
+                StudentId = x.StudentId,
+                StudentName = x.StudentName,
+                Email = x.Email,
+                DepartamentName = x.tblDepartament.DepartamentName
+            }).ToList();
+
+            return Json(StudentList, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MVCJquery/Models/StudentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MVCJquery.Models
+{
+    public class StudentViewModel
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+        public Nullable<int> DepartamentId { get; set; }
+
+        public string DepartamentName { get; set; }
+
+    }
+}
